f953a717736ca061fd2b65337a2b7d220ee0ef3a
nguyentrungphap <phapnguyen05032002@gmail.com>
2023-06-28 18:42:51 +0700

    buoi 1

---
+++ src/Youtubedata.js
@@ -0,0 +1,26 @@
+export const Youtubedata = [
+  {
+    id: 1,
+    images: "https://haycafe.vn/wp-content/uploads/2022/01/Hinh-anh-cute.jpg",
+    title: "reactjs",
+    author: "pháp nguyễn",
+  },
+  {
+    id: 2,
+    images: "https://haycafe.vn/wp-content/uploads/2022/01/Hinh-anh-cute.jpg",
+    title: "reactjs",
+    author: "pháp nguyễn",
+  },
+  {
+    id: 3,
+    images: "https://haycafe.vn/wp-content/uploads/2022/01/Hinh-anh-cute.jpg",
+    title: "reactjs",
+    author: "pháp nguyễn",
+  },
+  {
+    id: 4,
+    images: "https://haycafe.vn/wp-content/uploads/2022/01/Hinh-anh-cute.jpg",
+    title: "reactjs",
+    author: "pháp nguyễn",
+  },
+];
